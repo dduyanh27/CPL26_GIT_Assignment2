6a16530c9d91acb6e6dbb1d460af55136cff018a
thanhnthe186221 <tienthanh22092004@gmail.com>
2026-01-30 14:31:46 +0700

    implement divide

--- Assignment2/Assignment2_Divide/Program.cs
+++ Assignment2/Assignment2_Divide/Program.cs
@@ -1,10 +1,32 @@
-﻿namespace Assignment2_Divide
+﻿using System;
+
+namespace Assignment2_Divide
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write("Nhap so thu nhat: ");
+            double a = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Nhap so thu hai: ");
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            double result = Calculate("DIVIDE", a, b);
+
+            Console.WriteLine($"Ket qua: {result}");
+            Console.ReadKey();
+        }
+
+        private static double Calculate(string sign, double arg1, double arg2)
+        {
+            switch (sign)
+            {
+                case "DIVIDE":
+                    return arg2 != 0 ? arg1 / arg2 : 0;
+                default:
+                    return 0;
+            }
         }
     }
 }
